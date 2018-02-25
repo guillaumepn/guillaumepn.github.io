d97bc2e5393f391fc3edda24b9a052277d6c7cda
guillaumepn <guillaumepn@free.fr>
2018-02-25 02:48:35 +0100

    new icons + started drag n drop

--- wooly-game/src/components/grid.js
+++ wooly-game/src/components/grid.js
@@ -1,18 +1,26 @@
 class Grid {
     // Properties
-    constructor(horizontalTiles, verticalTiles, tileWidth, tileHeight) {
+    constructor(horizontalTiles, verticalTiles, tileWidth, tileHeight, container) {
         this.horizontalTiles = horizontalTiles;
         this.verticalTiles = verticalTiles;
         this.tileWidth = tileWidth;
         this.tileHeight = tileHeight;
+        this.container = container;
     }
     // Methods
     draw() {
-        console.log(
-            `
-            ${this.horizontalTiles} et ${this.verticalTiles} 
-            `
-        );
+
+        for (let y = 0; y < this.verticalTiles; y++) {
+            for (let x = 0; x < this.horizontalTiles; x++) {
+                let tile = new PIXI.Graphics();
+                tile.lineStyle(1, 0x000000, 0.2);
+                tile.drawRect(0, 0, this.tileWidth, this.tileHeight);
+                tile.position.set(x * this.tileWidth, y * this.tileHeight);
+                this.container.addChild(tile);
+            }
+        }
+
+        console.log(`Grille de ${this.horizontalTiles} sur ${this.verticalTiles}`);
     }
 }
 
--- wooly-game/src/main.js
+++ wooly-game/src/main.js
@@ -16,35 +16,93 @@ require('pixi.js');
 /**
  * Composants
  *
- * @TODO Grid: dessine la grille des cases de la map
  * @type {Grid}
  */
 const Grid = require('./components/grid');
 
-let grid = new Grid(15, 10, 32, 32);
-grid.draw();
-
 // Affiche la version de Pixijs dans la console du navigateur
 PIXI.utils.sayHello();
 
-// Dimensions du canvas du jeu
-let renderer = PIXI.autoDetectRenderer(640, 480, {
-    transparent: true,
+/**
+ * App du jeu
+ * Dimensions du canvas par défaut: 800 * 600
+ */
+let app = new PIXI.Application({
+    width: 960,
+    height: 480,
     resolution: 1,
+    // transparent: true
+    backgroundColor: 0xffffff
 });
 
 // Ajouter le <canvas> du jeu dans la <div id='app'>
-document.getElementById('app').appendChild(renderer.view);
+document.getElementById('app').appendChild(app.view);
+
+// Création du container global
+let container = new PIXI.Container();
 
 // Création du container du niveau
-let stage = new PIXI.Container();
+let stage = new PIXI.Graphics();
+stage.beginFill(0xb8e994);
+stage.drawRect(0, 0, 640, (app.renderer.height - 32));
+
+// Création du container du menu
+let menu = new PIXI.Graphics();
+menu.beginFill(0xfad390);
+menu.drawRect(0, 0, 320, (app.renderer.height - 32));
+menu.position.set(640, 0);
+
+// Ajout des sous-containers dans le container global
+container.addChild(stage);
+container.addChild(menu);
+
+// Dessine la grille sur la map
+let grid = new Grid(20, 14, 32, 32, stage);
+grid.draw();
+
+// Barre des actions possibles (dans le menu en bas)
+let actions = new PIXI.Container();
+menu.addChild(actions);
 
 // Chargement des sprites/images
 PIXI.loader
-    .add("cat", "./src/assets/images/cat-sprite.png")
+    .add("cat", "./src/assets/images/a-cat.svg")
+    .add("forward", "./src/assets/images/forward.png")
+    .add("turnleft", "./src/assets/images/turnleft.png")
+    .add("turnright", "./src/assets/images/turnright.png")
+    .add("wait", "./src/assets/images/wait.png")
     .load(setup);
 
 let cat;
+let forward;
+let turnleft;
+let turnright;
+let wait;
+
+function onDragStart(event) {
+    // store a reference to the data
+    // the reason for this is because of multitouch
+    // we want to track the movement of this particular touch
+    this.data = event.data;
+    this.alpha = 0.5;
+    this.dragging = true;
+}
+
+function onDragEnd() {
+    this.alpha = 1;
+    this.dragging = false;
+    // set the interaction data to null
+    this.data = null;
+}
+
+function onDragMove() {
+    if (this.dragging) {
+        var newPosition = this.data.getLocalPosition(this.parent);
+        this.x = newPosition.x;
+        this.y = newPosition.y;
+    }
+}
+
 
 /**
  * setup: au chargement de la page / du niveau
@@ -55,10 +113,67 @@ function setup() {
         PIXI.loader.resources['cat'].texture
     );
 
+    // cat.scale.set(0.1, 0.1);
+
+    // Icones d'action
+    forward = new PIXI.Sprite(
+        PIXI.loader.resources['forward'].texture
+    );
+    forward.x = 0;
+    forward.interactive = true;
+    forward.buttonMode = true;
+
+    turnleft = new PIXI.Sprite(
+        PIXI.loader.resources['turnleft'].texture,
+    );
+    turnleft.x = 32;
+    turnleft.interactive = true;
+    turnleft.buttonMode = true;
+
+    turnright = new PIXI.Sprite(
+        PIXI.loader.resources['turnright'].texture
+    );
+    turnright.x = 64;
+    turnright.interactive = true;
+    turnright.buttonMode = true;
+
+    wait = new PIXI.Sprite(
+        PIXI.loader.resources['wait'].texture
+    );
+    wait.x = 96;
+    wait.interactive = true;
+    wait.buttonMode = true;
+
+    actions.addChild(forward, turnleft, turnright, wait);
+
+    actions.x = (actions.parent.width / 2) - (actions.width / 2);
+    actions.y = actions.parent.height - 128;
+
+    for (let action of actions.children) {
+        action
+            .on('pointerdown', onDragStart)
+            .on('pointerup', onDragEnd)
+            .on('pointerupoutside', onDragEnd)
+            .on('pointermove', onDragMove);
+    }
+
+
     // Définit le point d'origine du sprite (avec 0.5 => au milieu)
     // cat.anchor.set(0.5, 0.5);
-    cat.x = 64;
-    cat.y = 64;
+    cat.x = 0;
+    cat.y = 0;
+
+    // For mouse-only events
+
+    let mapText = new PIXI.Text('La map du jeu');
+    mapText.x = 20;
+    mapText.y = stage.height;
+    stage.addChild(mapText);
+
+    let menuText = new PIXI.Text('Le menu du jeu');
+    menuText.x = 20;
+    menuText.y = menu.height;
+    menu.addChild(menuText);
 
     // On le rend "interactif" pour pouvoir utiliser des fonctions comme click() sur le chat
     cat.interactive = true;
@@ -80,7 +195,7 @@ function setup() {
 function loop() {
     requestAnimationFrame(loop);
 
-    cat.rotation += 0.1;
+    // cat.x += 2;
 
-    renderer.render(stage);
+    app.renderer.render(container);
 }
